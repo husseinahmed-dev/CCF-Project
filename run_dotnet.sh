a31e93ed16772f036bb5e996c554fcb38f2c4fa5
husseinahmed-dev <hussein.a.shahab@gmail.com>
2023-05-16 16:43:20 +0000

    Updated env variables

--- run_dotnet.sh
+++ run_dotnet.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export VAULT_ADDR='http://127.0.0.1:8200'
-export VAULT_TOKEN='hvs.CAESIB2qGGKhVdbtop1EC5k-hfvUCmfjeVW3RDPHQvapv9NDGh4KHGh2cy5XYU1McFZMMnJlTmJRYjhSazlQREpDanQ'
+export VAULT_TOKEN='some-root-token'
+#export VAULT_NAMESPACE='admin'
 
 cd ProjectApi
 
